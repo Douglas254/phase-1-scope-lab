962cc5e72e34db18b7190e443c59c8ac8945ef1a
Douglas Obara <douglasobara97@gmail.com>
2022-06-10 17:33:48 +0300

    commit tree 4613ed81f568d0877fb1f1da616f596cbd98cb6e

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+/*declare customerName to be bob in global scope
+returns the customerName
+*/
+var customerName = "bob";
+
+function myFun() {
+  return customerName;
+}
+
+/* upperCaseCustomerName()
+   modifies the customerName variable*/
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+/*
+  setBestCustomer()
+  setBestCustomer
+*/
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+/*
+    overwriteBestCustomer()
+    overwrites the best customer
+*/
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+/*
+    changeLeastFavoriteCustomer()
+    unsuccessfully tries to reassign the least favorite customer
+*/
+const leastFavoriteCustomer = "itCouldBeMe";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "debtCustomer";
+}
+console.log(leastFavoriteCustomer); // itCouldBeMe
